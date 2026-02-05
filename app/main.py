2960ec9c2a5a93a1042c84ab3a7cf7a997e91d5c
Nisarga <nisargakshetty@gmail.com>
2026-02-05 11:36:05 +0530

    changes updated

--- app/main.py
+++ app/main.py
@@ -1,9 +1,12 @@
 from fastapi import FastAPI
+from dotenv import load_dotenv
+import os
+
+load_dotenv()   # 👈 THIS LINE FIXES EVERYTHING
+
 from app.routes.honeypot import router as honeypot_router
-from app.utils.auth import verify_api_key
 
 app = FastAPI(title="Agentic Honeypot API")
-
 
 app.include_router(honeypot_router, prefix="/honeypot")
 
